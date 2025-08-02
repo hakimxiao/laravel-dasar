e5caaba1ba02a8b40c525466f6ef20f268ddd61a
abbu solihin alhakim <alhakimabusolihin@gmail.com>
2025-08-02 23:40:48 +0700

    Protected Route Pada Laravel || Menggunakan midleware auth atau Middleware guest || Serta melakukkan grouping terhadap route untuk mengcleaning code supaya middleware dan controller nya cukup di deklarasikan satu kali

--- routes/web.php
+++ routes/web.php
@@ -9,26 +9,65 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// # - - - AUTH START - - - #
-// * Route Handler AuthUI
-Route::get('/auth/login', [AuthController::class, 'showLogin'])->name('auth.login');
 
-Route::get('/auth/register', [AuthController::class, 'showRegister'])->name('auth.register');
 
-// Route Handler AuthLogic
-Route::post('/auth/login', [AuthController::class, 'login'])->name('login');
+// // # GROUPING MIDDLEWARE ROUTE PROTECTED #
+// Route::middleware('auth')->group(function () {
+//     Route::get('/siswa', [SiswaController::class, 'index'])->name('siswa.index');
 
-Route::post('/auth/register', [AuthController::class, 'register'])->name('register');
+//     Route::post('/siswa', [SiswaController::class, 'store'])->name('siswa.store');
+
+//     Route::get('/siswa/create', [SiswaController::class, 'create'])->name('siswa.create');
+
+//     Route::get('/siswa/{siswa}', [SiswaController::class, 'show'])->name('siswa.show');
+
+//     Route::delete('/siswa/{siswa}', [SiswaController::class, 'destroy'])->name('siswa.destroy');
+// });
+// // # GROUPING MIDDLEWARE ROUTE PROTECTED #
+
 
 Route::post(('/auth/logout'), [AuthController::class, 'logout'])->name('logout');
-// # - - - AUTH END - - - #
 
-Route::get('/siswa', [SiswaController::class, 'index'])->name('siswa.index');
 
-Route::post('/siswa', [SiswaController::class, 'store'])->name('siswa.store');
+// Guest middleware : Adalah middleware yang akan sepesialis menangasni sesuaitu yang dimana user itu belum login
+Route::middleware('guest')->controller(AuthController::class)->group(function() {
+    // # - - - AUTH START - - - #
+    // * Route Handler AuthUI
+    Route::get('/auth/login', 'showLogin')->name('auth.login');
 
-Route::get('/siswa/create', [SiswaController::class, 'create'])->name('siswa.create');
+    Route::get('/auth/register', 'showRegister')->name('auth.register');
 
-Route::get('/siswa/{siswa}', [SiswaController::class, 'show'])->name('siswa.show');
+    // Route Handler AuthLogic
+    Route::post('/auth/login', 'login')->name('login');
 
-Route::delete('/siswa/{siswa}', [SiswaController::class, 'destroy'])->name('siswa.destroy');
+    Route::post('/auth/register', 'register')->name('register');
+
+    // # - - - AUTH END - - - #
+});
+
+
+
+// Group Auth  + Group Controller karena semua controller sama :
+Route::middleware('auth')->controller(SiswaController::class)->group(function () {
+    Route::get('/siswa', 'index')->name('siswa.index');
+
+    Route::post('/siswa', 'store')->name('siswa.store');
+
+    Route::get('/siswa/create', 'create')->name('siswa.create');
+
+    Route::get('/siswa/{siswa}', 'show')->name('siswa.show');
+
+    Route::delete('/siswa/{siswa}', 'destroy')->name('siswa.destroy');
+});
+
+
+
+// Route::get('/siswa', [SiswaController::class, 'index'])->name('siswa.index');
+
+// Route::post('/siswa', [SiswaController::class, 'store'])->name('siswa.store');
+
+// Route::get('/siswa/create', [SiswaController::class, 'create'])->name('siswa.create');
+
+// Route::get('/siswa/{siswa}', [SiswaController::class, 'show'])->name('siswa.show');
+
+// Route::delete('/siswa/{siswa}', [SiswaController::class, 'destroy'])->name('siswa.destroy');
